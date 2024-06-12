067e6242a9b5388905992a50d2bc433fa92187f1
AbdelazizMaher <abdelazizmaher17499@gmail.com>
2024-06-12 21:23:03 +0300

    add turtlesim_catch_them_all

---
+++ py_turtlesim_catch_them_all/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'py_turtlesim_catch_them_all'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='abdelaziz',
+    maintainer_email='abdelazizmaher17499@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
