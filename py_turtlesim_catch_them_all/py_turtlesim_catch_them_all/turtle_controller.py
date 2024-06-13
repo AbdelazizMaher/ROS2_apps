4e3268b7215696637ede81ce99f5ace77f2e7ee2
AbdelazizMaher <abdelazizmaher17499@gmail.com>
2024-06-13 12:35:34 +0300

    catch alive turtles

--- py_turtlesim_catch_them_all/py_turtlesim_catch_them_all/turtle_controller.py
+++ py_turtlesim_catch_them_all/py_turtlesim_catch_them_all/turtle_controller.py
@@ -8,29 +8,38 @@ import math
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 
+from my_custom_interfaces.msg import Turtle
+from my_custom_interfaces.msg import TurtleArray
+from my_custom_interfaces.srv import CatchTurtle
+
 class TurtleControllerNode(Node):
     
     def __init__(self):
         super().__init__("turtle_controller")
         
-        self.target_x = 4
-        self.target_y = 8
-        
+        self.turtle_to_catch_ = None
         self.pose_ = None
         
-        self.new_pos_publisher_ = self.create_publisher(Twist,"turtle1/cmd_vel",10)
-        self.turtle_pos_subscriber_ = self.create_subscription(Pose,"turtle1/pose",self.callbackNewPos)
+        self.new_pos_publisher_ = self.create_publisher(Twist, "turtle1/cmd_vel", 10)
+        self.turtle_pos_subscriber_ = self.create_subscription(Pose, "turtle1/pose", self.callbackNewPos)
+        self.alive_turtles_subscriber_ = self.create_subscription(TurtleArray, "alive_turtles", self.callbackALiveTurtles)
         self.control_loop_timer_ = self.create_timer(0.01,self.callbackControlPos)
     
+    
+    def callbackALiveTurtles(self, msg):
+        if len(msg.turtles) > 0:
+            self.turtle_to_catch_ = msg.turtles[0]
+        
+        
     def callbackNewPos(self,msg):
         self.pose_ = msg    
      
     def callbackControlPos(self):
-        if self.pose_ == None:
+        if self.pose_ == None or self.turtle_to_catch_ == None:
             return
         
-        dist_x = self.target_x - self.pose_.x
-        dist_y = self.target_y - self.pose_.y
+        dist_x = self.turtle_to_catch_.x - self.pose_.x
+        dist_y = self.turtle_to_catch_.y - self.pose_.y
         distance = math.sqrt(dist_x * dist_x + dist_y * dist_y)
         
         msg = Twist()
--- py_turtlesim_catch_them_all/py_turtlesim_catch_them_all/turtle_spawner.py
+++ py_turtlesim_catch_them_all/py_turtlesim_catch_them_all/turtle_spawner.py
@@ -10,6 +10,10 @@ from rclpy.node import Node
 from turtlesim.srv import Spawn
 from turtlesim.srv import Kill
 
+from my_custom_interfaces.msg import Turtle
+from my_custom_interfaces.msg import TurtleArray
+from my_custom_interfaces.srv import CatchTurtle
+
 class TurtleSpawner(Node):
     
     def __init__(self):
@@ -20,18 +24,25 @@ class TurtleSpawner(Node):
         
         self.spawn_frequency_ = self.get_parameter("spawn_frequency").value
         self.turtle_name_prefix_ = self.get_parameter("turtle_name_prefix").value
-            
+        
+        self.alive_turtles_ = []    
         self.turtle_counter_ = 0
         
+        self.alive_turtles_publisher_ = self.create_publisher(TurtleArray, "alive_turtles", 10)         
         self.spawn_turtle_timer_ = self.create_timer(1.0/self.spawn_frequency, self.spawn_new_turtle) 
+
+    def publish_alive_turtles(self):
+        msg = TurtleArray()
+        msg.turtles = self.alive_turtles_
         
+        self.alive_turtles_publisher_.publish(msg)
+            
     def spawn_new_turtle(self):
         self.turtle_counter += 1
+        
         turtle_name = self.turtle_name_prefix_ + str(self.turtle_counter_)
-        
         x = random.uniform(0.0, 11.0)
-        y = random.uniform(0.0, 11.0)
-        
+        y = random.uniform(0.0, 11.0)   
         theta = random.uniform(0.0, 2*math.pi) 
                
         self.callTurtleSpawner_server(x, y, theta, turtle_name)    
@@ -41,15 +52,13 @@ class TurtleSpawner(Node):
         while not client.wait_for_service(1.0):
             self.get_logger().info("Waiting for Server...") 
         
-        request = Spawn.Request()
-        
+        request = Spawn.Request()        
         request.x = x
         request.y = y
         request.theta = theta
         request.name =  turtle_name
         
-        future = client.call_async(request)
-        
+        future = client.call_async(request)        
         future.add_done_callback(
             partial(self.callback_call_spawn, x=x, y=y, theta=theta, turtle_name=turtle_name))  
         
@@ -58,7 +67,15 @@ class TurtleSpawner(Node):
             response = future.result()
             if response.name != "":
                 self.get_logger().info("Turtle " + response.name + " is now alive")
-            
+                
+                new_alive_turtle = Turtle()
+                new_alive_turtle.x = x
+                new_alive_turtle.y = y
+                new_alive_turtle.theta = theta
+                new_alive_turtle.name = turtle_name
+                self.alive_turtles_.append(new_alive_turtle)
+                
+                self.publish_alive_turtles()                
         except Exception as e:
             self.get_logger().error("Service call failed %r" % (e,))
                 
