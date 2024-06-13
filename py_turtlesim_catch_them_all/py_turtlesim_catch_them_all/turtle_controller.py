0352c708dfc6f34c410a43741d254dc25be79ef3
AbdelazizMaher <abdelazizmaher17499@gmail.com>
2024-06-13 19:19:35 +0300

    catch closest turtle

--- py_turtlesim_catch_them_all/py_turtlesim_catch_them_all/turtle_controller.py
+++ py_turtlesim_catch_them_all/py_turtlesim_catch_them_all/turtle_controller.py
@@ -18,6 +18,9 @@ class TurtleControllerNode(Node):
     def __init__(self):
         super().__init__("turtle_controller")
         
+        self.declare_parameter("catch_closest_turtle_first", True)
+        self.catch_closest_turtle_first_ = self.get_parameter("catch_closest_turtle_first").value 
+                   
         self.turtle_to_catch_ = None
         self.pose_ = None
         
@@ -29,7 +32,20 @@ class TurtleControllerNode(Node):
     
     def callbackALiveTurtles(self, msg):
         if len(msg.turtles) > 0:
-            self.turtle_to_catch_ = msg.turtles[0]
+            if self.catch_closest_turtle_first_:
+                closest_turtle = None
+                closest_turtle_distance = None
+                for turtle in msg.turtles:
+                    dist_x = turtle.x - self.pose_.x
+                    dist_y = turtle.y - self.pose_.y
+                    distance = math.sqrt(dist_x * dist_x + dist_y * dist_y)
+                    
+                    if closest_turtle == None or closest_turtle_distance == None:
+                        closest_turtle = turtle
+                        closest_turtle_distance = distance 
+                self.turtle_to_catch_ = closest_turtle                   
+            else:
+                self.turtle_to_catch_ = msg.turtles[0]
         
         
     def callbackNewPos(self,msg):
